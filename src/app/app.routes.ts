3f967b9981080ab687a826d856b091ac5dfca7f6
DanRuizPeru <u202210764@upc.edu.pe>
2025-10-10 08:47:19 -0500

    add camera bounded context

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,19 +2,47 @@ import { Routes } from '@angular/router';
 import { LoginForm } from './iam/components/login-form/login-form';
 import { SigninForm } from './iam/components/signin-form/signin-form';
 import { UserLayout } from './iam/presentation/views/user-layout/user-layout';
-import { HomeComponent } from './public/presentation/views/home-component/home-component';
+import { HomeComponent } from './public/components/home-component/home-component';
+import { ConfigComponent } from './public/components/config-component/config-component';
+import { BaseLayout } from './public/presentation/views/base-leyout/base-layout';
+import { SupportLayout } from './public/presentation/views/support-layout/support-layout';
+import { FrequentQuestions } from './public/components/frequent-questions/frequent-questions';
+import { HumanSuport } from './public/components/human-suport/human-suport';
+
 
 export const routes: Routes = [
+  // 🔹 LOGIN / SIGNIN layout
   {
     path: '',
     component: UserLayout,
     children: [
-      { path: '', redirectTo: 'login', pathMatch: 'full' }, // Ruta vacía predeterminada
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
       { path: 'login', component: LoginForm },
       { path: 'signin', component: SigninForm },
     ],
   },
-  { path: 'home', component: HomeComponent, pathMatch: 'full' },
+
+  // 🔹 LAYOUT PRINCIPAL
+  {
+    path: '',
+    component: BaseLayout,
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: 'settings', component: ConfigComponent },
+    ],
+  },
+
+  // 🔹 SOPORTE layout
+  {
+    path: 'assistance',
+    component: SupportLayout,
+    children: [
+      { path: '', redirectTo: 'frequent-questions', pathMatch: 'full' },
+      { path: 'frequent-questions', component: FrequentQuestions },
+      { path: 'human-suport', component: HumanSuport },
+
+    ],
+  },
   {
     path: 'cameras',
     loadComponent: () => import('./cameras/presentation/views/cameras-dashboard/cameras-dashboard.component')
@@ -25,5 +53,9 @@ export const routes: Routes = [
     loadComponent: () => import('./cameras/presentation/views/camera-settings/camera-settings.component')
       .then(c => c.CameraSettingsComponent)
   },
-  { path: '**', redirectTo: '' } // wildcard global al final
+
+
+
+  // 🔹 WILDCARD
+  { path: '**', redirectTo: 'login' },
 ];
